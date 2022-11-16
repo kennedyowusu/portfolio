845fd83f0452e643256f96fa7c2bf5556910af69
Kennedy Owusu <kennedyowusu@gmail.com>
2022-11-16 08:22:07 +0000

    resolve linter errors

--- script.js
+++ script.js
@@ -13,9 +13,3 @@ document.querySelectorAll('.hide-mobile-menu').forEach((n) => n.addEventListener
   navMenu.classList.remove('active');
   mobileMenuList.classList.remove('hide-mobile-menu');
 }));
-
-document.querySelectorAll('.hide-mobile-menu').forEach((n) => n.addEventListener('click', () => {
-  hamburger.classList.add('active');
-  navMenu.classList.add('active');
-  mobileMenuList.classList.add('hide-mobile-menu');
-}));
